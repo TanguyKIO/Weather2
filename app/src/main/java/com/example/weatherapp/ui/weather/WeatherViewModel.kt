57f306b0be75a2b54a19926cefed6bed94ca48eb
TanguyKIO <tang.caillaud@gmail.com>
2020-09-09 17:03:12 +0200

    API + un peu de front refresh a faire

--- app/src/main/java/com/example/weatherapp/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/weather/WeatherViewModel.kt
@@ -1,22 +1,25 @@
 package com.example.weatherapp.ui.weather
 
-import android.util.Log
+import WeatherData
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.weatherapp.data.Weather
-import com.example.weatherapp.data.WeatherType
+import com.example.weatherapp.data.WeatherRepository
+import javax.inject.Inject
 
 private const val TAG = "WeatherViewModel"
 
-class WeatherViewModel : ViewModel() {
+class WeatherViewModel @Inject constructor(
+) : ViewModel() {
 
-    private val _weather = MutableLiveData<Weather>()
-    val weather: LiveData<Weather> = _weather
+    private val weatherRepository = WeatherRepository()
+    private var _weatherData = weatherRepository.getWeather()
+    //private val liveDataMerger = MediatorLiveData<WeatherData>()
+    val weatherData: LiveData<WeatherData> = _weatherData
+    //liveDataMerger.addSource(_weatherData)
+    /*fun update() {
+        _weatherData = weatherRepository.getWeather()
 
-    fun update(x: Int) {
-        val newWeather = Weather(type = WeatherType.CLOUDY, temperature = x)
-        _weather.postValue(newWeather)
-        Log.e(TAG, "weather set: $newWeather")
-    }
+    }*/
 }
